a2551aeecd56bab1e5d4abcd5188987bb8ee9429
Debargha Mondal <166617838+DebarghaMondal@users.noreply.github.com>
2024-08-14 10:21:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,41 +1,33 @@
 class Person {
   constructor(name, age) {
-    this.name = name;
-    this.age = age;
+    this._name = name;
+    this._age = age;
   }
 
   get name() {
-    return this.name;
+    return this._name;
   }
 
   set age(age) {
-    this.age = age;
+    this._age = age;
   }
-	  get age() {
+
+  get age() {
     return this._age;
   }
 }
 
 class Student extends Person {
-  constructor(name, age) {
-    super(name, age);
-  }
-
   study() {
-    console.log(`${this.name} is studying`);
+    console.log(`${this._name} is studying`);
   }
 }
 
 class Teacher extends Person {
-  constructor(name, age) {
-    super(name, age);
-  }
-
   teach() {
-    console.log(`${this.name} is teaching`);
+    console.log(`${this._name} is teaching`);
   }
 }
-
 // Do not change the code below this line
 window.Person = Person;
 window.Student = Student;
